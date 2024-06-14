164e63aa511bf0f9ad1df0e0bb21b1fe4a33beb3
Angel Castro <castrobasurto.angel@gmail.com>
2024-06-14 17:41:20 -0400

    added gw

--- example/variables.tf
+++ example/variables.tf
@@ -30,6 +30,7 @@ variable "containers" {
             unprivileged    = true
             ip              = "192.168.0.200/24"
             vmid            = 200
+            gw              = "192.168.0.1"
         }
     ]
 }
