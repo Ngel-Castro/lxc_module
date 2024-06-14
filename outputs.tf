7350f301f96f152a49d60ead725ac3c55a5b2584
Angel Castro <castrobasurto.angel@gmail.com>
2024-06-14 17:18:15 -0400

    added ip

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "container_vmids" {
   value = { for k, v in proxmox_lxc.container : k => v.vmid }
 }
+
+output "container_ips" {
+  value = { for k, v in proxmox_lxc.container : k => v.network[0].ip }
+}
